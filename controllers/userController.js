ef360b04a57e906c8f25eeb1c9061159530486b3
Kevin Biliguun <ktb53@cornell.edu>
2024-12-07 05:06:17 -0500

    Changed some debugging statements

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,7 +64,7 @@ const loginUser = async (req, res) => {
     // Compare the provided password with the stored hashed password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: 'Password not working gangalicious' });
+      return res.status(400).json({ error: 'Password not working gangalicious', providedPassword: password });
     }
 
     // Generate a response indicating successful login
